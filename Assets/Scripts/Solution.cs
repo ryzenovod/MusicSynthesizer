25289730a178215c548f47e6b2f64192ee460ce3
ryzenovod <88344639+ryzenovod@users.noreply.github.com>
2025-11-23 13:56:45 +1000

    Fix Solution lifecycle method signatures

--- Assets/Scripts/Solution.cs
+++ Assets/Scripts/Solution.cs
@@ -7,12 +7,12 @@ public class Solution : MonoBehaviour
     public SceneControlScript controller;
     private bool solutionExecuted = false;
 
-    public override void Start()
+    public void Start()
     {
         controller = gameObject.GetComponent<SceneControlScript>();
     }
 
-    public override void Update()
+    public void Update()
     {
         if (!solutionExecuted && !controller.IsPlaying())
         {
@@ -30,6 +30,30 @@ public class Solution : MonoBehaviour
 
     private int CalculateTotalTacts()
     {
-        return 0;
+        int totalNotes = controller.GetTotalNotes();
+        int[] limits = new int[totalNotes];
+
+        for (int i = 0; i < totalNotes; i++)
+        {
+            limits[i] = controller.GetNoteLimit(i);
+        }
+
+        int alpha = 1;
+        int beta = 1;
+        int currentPosition = 0;
+        int gamma = 0;
+
+        while (limits[currentPosition] > 0)
+        {
+            limits[currentPosition]--;
+            gamma++;
+
+            int delta = (alpha + beta) % totalNotes;
+            alpha = beta;
+            beta = delta;
+            currentPosition = delta;
+        }
+
+        return gamma;
     }
 }
